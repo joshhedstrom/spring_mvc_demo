b0fea39c66d687ca140794cb1414a8e31eb9db61
Josh Hedstrom <josh.hedstrom@gmail.com>
2018-09-25 15:53:15 -0600

    added serving static pdf functionality

--- src/main/java/com/pluralsight/controller/MinutesController.java
+++ src/main/java/com/pluralsight/controller/MinutesController.java
@@ -16,4 +16,11 @@ public class MinutesController {
 		return "addMinutes";
 	}
 
+//	@RequestMapping(value = "/addMoreMinutes")
+//	public String addMoreMinutes(@ModelAttribute("exercise") Exercise exercise) {
+//		System.out.println("Exercising: " + exercise.getMinutes());
+//
+//		return "addMinutes";
+//	}
+
 }
